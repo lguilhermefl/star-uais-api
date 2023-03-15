a9d24ffeadef3d746fcd198fda5b32ed52acab8e
luiz <lguilhermefl@gmail.com>
2023-03-14 22:10:41 -0300

    feat: add validator to auth dto

--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,4 +1,11 @@
-export class AuthDto {
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
+export class AuthDefaultDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @Length(8, 20)
+  @IsNotEmpty()
   password: string;
 }
