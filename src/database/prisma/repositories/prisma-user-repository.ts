fbc77a75083c43662529209204b9e900f52c1c32
luiz <lguilhermefl@gmail.com>
2023-03-14 21:57:27 -0300

    feat: add prisma user repository

---
+++ src/database/prisma/repositories/prisma-user-repository.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../../../users/dtos/create-user.dto';
+import { UpdateUserDto } from '../../../users/dtos/update-user.dto';
+import { User } from '../../../users/entities/user';
+import { UsersRepository } from '../../../users/repositories/users-repository';
+import { PrismaUserMapper } from '../mappers/prisma-user-mapper';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = await this.prisma.user.create({
+      data: createUserDto,
+    });
+
+    return PrismaUserMapper.toDomain(createdUser);
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) return null;
+
+    return PrismaUserMapper.toDomain(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) return null;
+
+    return PrismaUserMapper.toDomain(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    const users = await this.prisma.user.findMany();
+
+    return users.map((user) => PrismaUserMapper.toDomain(user));
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const updatedUser = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        ...updateUserDto,
+      },
+    });
+
+    return PrismaUserMapper.toDomain(updatedUser);
+  }
+}
