2209b03ed941a9945d96ae3f5f4fd9b2d5b01adc
luiz <lguilhermefl@gmail.com>
2023-03-14 21:47:10 -0300

    feat: use cors and global pipes

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
+  const logger = new Logger('main');
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<string>('PORT') ?? '3000';
+  await app.listen(PORT, () => logger.log(`Server is running on port ${PORT}`));
+}
+bootstrap();
